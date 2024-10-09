3df385bee7a78a5415c419f3f841ccbc12a813ab
JohannesLagnefeldt <johannes.lagnefeldt@gmail.com>
2024-10-09 21:05:45 +0200

    Bord och skit

---
+++ Assets/Scripts/TableRender.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableRender : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform midPoint;
+    [SerializeField] private int layerRelativeToOtherTables;
+
+    private SpriteRenderer[] spriteRenderers;
+
+    private void Start()
+    {
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if(midPoint.position.y < player.transform.position.y)
+        {
+            foreach(SpriteRenderer spriteRenderer in spriteRenderers)
+            {
+                spriteRenderer.sortingOrder = layerRelativeToOtherTables + 5;
+            }
+        }
+        if (midPoint.position.y > player.transform.position.y)
+        {
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+            {
+                spriteRenderer.sortingOrder = layerRelativeToOtherTables - 5;
+            }
+        }
+    }
+}
