7d92126c8c1f32531d0220908765e37f6a481f9d
JohannesLagnefeldt <johannes.lagnefeldt@gmail.com>
2024-10-10 23:14:59 +0200

    More things

---
+++ Assets/Scripts/MilkMachine.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkMachine : Interactable
+{
+    public override void Interact(PlayerCharacter player)
+    {
+        if (player.inventory[0] == PlayerCharacter.Item.CUP_BLACK)
+        {
+            player.inventory[0] = PlayerCharacter.Item.CUP_BLACK;
+            print(player.inventory[0]);
+            print(player.inventory[1]);
+        }
+        else if (player.inventory[1] == PlayerCharacter.Item.NOTHING)
+        {
+            player.inventory[1] = PlayerCharacter.Item.CUP_BLACK;
+            print(player.inventory[0]);
+            print(player.inventory[1]);
+        }
+
+        player.UpdateTray();
+    }
+}
